48065f5444712be6c0699a6771e0c19fa235111f
luddedud <ludwig.gustafsson@elev.ga.ntig.se>
2025-11-25 11:02:46 +0100

    Databas grund fixad klassdigram och kravspec

---
+++ Project3/Databas/Database.sql
@@ -0,0 +1,28 @@
+CREATE TABLE Users (
+    UserID INT PRIMARY KEY identity(1,1),
+    Name NVARCHAR(100) NOT NULL,
+    Email NVARCHAR(100) NOT NULL UNIQUE,
+    IsAdmin BIT NOT NULL
+);
+
+CREATE TABLE TicketStatus (
+    id INT PRIMARY KEY identity(1,1),
+    Name NVARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Tickets (
+    id INT PRIMARY KEY identity(1,1),
+    Title NVARCHAR(200) NOT NULL,
+    Description NVARCHAR(MAX),
+    UserId INT NOT NULL,
+    StatusId INT NOT NULL,
+    CreatedAt DATETIME NOT NULL DEFAULT GETDATE(),
+    FOREIGN KEY (UserId) REFERENCES Users(Id),
+    FOREIGN KEY (StatusId) REFERENCES TicketStatus(id)
+)
+
+INSERT INTO TicketStatus (Name) VALUES 
+('Öppen'),
+('Pågående'),
+('Löst'),
+('Stängd');
---
+++ Project3/doc/kravspec.md
@@ -0,0 +1,11 @@
+# Kravspecifikation
+
+## User
+- User ska kunna skapa en ticket.
+- User ska kunna se sina egna tickets.
+- User ska kunna se ticketstatus.
+
+## Admin
+- Admin ska kunna se alla tickets.
+- Admin ska kunna ändra ticketstatus.
+- Admin ska kunna tilldela ticket till en user.
